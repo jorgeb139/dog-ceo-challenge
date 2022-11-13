1bab3cf52574de42cee2950333b122d5e7fe8847
Jorge Burguera <jburguera@meatcode.cl>
2022-11-13 16:02:34 -0300

    Feat: Add Hero styles

--- src/components/doggiesSearch/index.js
+++ src/components/doggiesSearch/index.js
@@ -2,18 +2,25 @@ import React from "react";
 import { Grid, Box } from "@mui/material";
 
 import "./styles.css";
-import PrincipalImage from "./PrincipalImage";
 import InputSearch from "./InputSearch";
 
 export default function DoggiesSearch() {
   return (
-    <Grid container justifyContent="center" paddingTop={6}>
-      <Grid item xs={12}>
-        <PrincipalImage />
-        <div className={"inputContainer"}>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container justifyContent="center" paddingTop={6}>
+        <Grid className="hero-container" item xs={12}>
+          <h2 className="pre-title">Discover the most beautiful</h2>
+          <h1 className="main-title">Doggies</h1>
+          <img
+            className="hero-image"
+            src="/images/hero-doggie.png"
+            alt="hero doggie"
+          />
+        </Grid>
+        <Grid className={"input-container"}>
           <InputSearch />
-        </div>
+        </Grid>
       </Grid>
-    </Grid>
+    </Box>
   );
 }
